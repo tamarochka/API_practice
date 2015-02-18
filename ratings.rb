0e0a2acd89160315e105238a7a8c744268cd7912
tamarochka <tamara.zenkevich@gmail.com>
2015-02-17 23:46:32 -0500

    created new hash with ratings from API

--- ratings.rb
+++ ratings.rb
@@ -6,27 +6,39 @@ require "csv"
 
 class OMDbApi
 
- def self.connect_api
-  query = ""
-  @result = JSON.parse(Net::HTTP.get(URI.parse(URI.escape(query))))
+ def self.connect_api(movie)
+  query = "http://www.omdbapi.com/?t=#{movie["title"]}&y=#{movie["year"]}&plot=short&r=json"
+  result = JSON.parse(Net::HTTP.get(URI.parse(URI.escape(query))))
 end
 
-def errors?
-
+def self.errors?(movie)
+  false
 end
 
-def movie_rating
-
+def self.movie_rating(movie)
+  movie = connect_api(movie)
+  movie["imdbRating"]
 end
+
 end
 
 def read_file_from(filename)
   movies = []
-
   CSV.foreach(filename, headers: true) do |row|
     movies << row.to_hash
   end
   movies
 end
 
-puts read_file_from("movies.csv")
+movies_with_ratings = []
+read_file_from("movies.csv").each do |movie|
+  if OMDbApi.errors?(movie)
+    puts "something went wrong"
+  else movie["rating"] = OMDbApi.movie_rating(movie)
+    movies_with_ratings << movie
+  end
+end
+
+movies_with_ratings.each do |movie|
+  puts "Title: #{movie["title"]} Year: #{movie["rating"]}"
+end
